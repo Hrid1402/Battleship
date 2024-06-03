358c8c96319a8d4b90fcec9ab5a347a7f10e57de
Hrid1402 <Hrid1402@gmail.com>
2024-06-02 19:32:24 -0500

    feat: working on ship and board Classes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
 import("./style.css");
 
 console.log("Working");
+class Ship{
+    constructor(length){
+        this.length = length;
+        this.curHits = 0;
+        this.sunk = false;
+    }
+    hit(){
+        curHits++;
+    }
+    isSunk(){
+        if(curHits == length){
+            sunk = true;
+        }
+    }
+}
+class Gameboard{
+    constructor(){
+        this.board = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ];
+    }
+    receiveAttack(x,y){
+        console.log("x");
+    }
+    addShip(ship, row, column, direction){
+        
+        for(let i=0; i<ship.length; i++){
+            if(direction=="h"){
+                this.board[row][column+i]=1;
+            }else{
+                this.board[row+i][column]=1;
+            }
+            
+        }
+        console.log(this.board);
+    }
+}
+const myGameboard = new Gameboard();
+myShip = new Ship(1);
+console.log(myShip);
+myGameboard.addShip(myShip, row=4, column=0, "v");
+//console.log(myGameboard.board);
