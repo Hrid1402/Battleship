9f10c14ab126de51d2b6752f51eee88831108a79
Hrid1402 <Hrid1402@gmail.com>
2024-06-22 17:07:25 -0500

    feat: drag almost working

--- src/index.js
+++ src/index.js
@@ -1,54 +1,103 @@
 import("./style.css");
-
 console.log("Working");
-class Ship{
-    constructor(length){
-        this.length = length;
-        this.curHits = 0;
-        this.sunk = false;
+const carrierSVG = document.querySelector(".carrierSVG");
+const CELLS = document.querySelectorAll(".cell");
+console.log(CELLS);
+console.log(carrierSVG);
+
+carrierSVG.addEventListener("dragstart", () => {
+    carrierSVG.classList.add("dragging");
+    const dragginShip = document.querySelector(".dragging");
+    setTimeout(() => {
+        dragginShip.classList.add('dragging');
+        dragginShip.style.pointerEvents = 'none'; // Desactiva los eventos del puntero para el elemento arrastrado
+      }, 0);
+    //carrierSVG.remove();
+});
+carrierSVG.addEventListener("dragend", () => {
+    carrierSVG.classList.remove("dragging");
+    carrierSVG.style.pointerEvents = '';
+});
+CELLS.forEach((cell) => {
+    cell.addEventListener("dragover", (e) => {
+        e.preventDefault();
+        const dragginShip = document.querySelector(".dragging");
+        cell.appendChild(dragginShip);
+        cell.classList.add("inFront");
+    });
+    cell.addEventListener("dragleave", () => {
+        cell.classList.remove("inFront");
+    });
+});
+
+class Ship {
+  constructor(length) {
+    this.length = length;
+    this.curHits = 0;
+    this.sunk = false;
+  }
+  hit() {
+    this.curHits++;
+    console.log("current Hits +1");
+  }
+  isSunk() {
+    if (this.curHits == this.length) {
+      this.sunk = true;
+      console.log("SUNK");
     }
-    hit(){
-        curHits++;
-    }
-    isSunk(){
-        if(curHits == length){
-            sunk = true;
-        }
-    }
+  }
 }
-class Gameboard{
-    constructor(){
-        this.board = [
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        ];
+class Gameboard {
+  constructor() {
+    this.board = [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+  }
+  receiveAttack(row, column) {
+    if (this.board[row][column] == 1) {
+      this.board[row][column] = "x";
+      console.log("hit");
+      console.log(ships[1].curHits);
+      ships[1].hit();
+      ships[1].isSunk();
+    } else {
+      console.log("miss");
     }
-    receiveAttack(x,y){
-        console.log("x");
+  }
+  addShip(ship, row, column, direction) {
+    for (let i = 0; i < ship.length; i++) {
+      if (direction == "h") {
+        this.board[row][column + i] = 1;
+      } else {
+        this.board[row + i][column] = 1;
+      }
     }
-    addShip(ship, row, column, direction){
-        
-        for(let i=0; i<ship.length; i++){
-            if(direction=="h"){
-                this.board[row][column+i]=1;
-            }else{
-                this.board[row+i][column]=1;
-            }
-            
-        }
-        console.log(this.board);
-    }
+    console.log(this.board);
+  }
 }
 const myGameboard = new Gameboard();
-myShip = new Ship(1);
-console.log(myShip);
-myGameboard.addShip(myShip, row=4, column=0, "v");
+ship1 = new Ship(5); //Carrier
+ship2 = new Ship(4); //Battleship
+ship3 = new Ship(3); //Cruiser
+ship4 = new Ship(3); //Submarine
+ship5 = new Ship(2); //Destroyer
+let ships = {
+    1 : ship1,
+    2 : ship2,
+    3 : ship3,
+    4 : ship4,
+    5 : ship5
+};
+myGameboard.addShip(ships[1], (row = 4), (column = 0), "v");
+myGameboard.addShip(new Ship(4), (row = 0), (column = 0), "h");
+myGameboard.receiveAttack(0, 0);
 //console.log(myGameboard.board);
